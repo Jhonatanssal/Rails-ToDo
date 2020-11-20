19b001fa9c7552993a2c0945edf0f65afad8ca74
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-11-19 21:06:28 -0500

    Add methods to Tasks model Add select options

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,37 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  validates :status, inclusion: { in: ['incomplete', 'complete'] }
+  validates :priority, inclusion: { in: ['high-priority', 'mid-priority', 'low-priority']}
+
+  STATUS_OPTIONS = [['Incomplete', 'incomplete'], ['Complete', 'complete']]
+
+  PRIORITY_OPTIONS = [
+    ['High Priority', 'high-priority'],
+    ['Mid Priority', 'mid-priority'],
+    ['Low Priority', 'low-priority']
+  ]
+
+  def badge_color1
+    case priority
+    when 'high-priority'
+      'danger'
+    when 'mid-priority'
+      'warning'
+    else
+      'primary'
+    end
+  end
+
+  def badge_color2
+    case status
+    when 'incomplete'
+      'warning'
+    when 'complete'
+      'success'
+    else
+      'primary'
+    end
+  end
+  
 end
